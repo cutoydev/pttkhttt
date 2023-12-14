b14086b78377ca1df8a5bc85c6e1c6ffaf0377a7
Trần Cao Nguyên <121510966+cutoydev@users.noreply.github.com>
2023-12-14 21:23:52 +0700

    Add files via upload

---
+++ sachhayhuce/layout/footer.php
@@ -0,0 +1,90 @@
+<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2>Sachhayhuce</h2>
+							<p>Sắc đẹp chỉ tồn tại một thời gian, còn tri thức bên ta cả đời</p>
+						</div>
+					</div>
+										
+					<div class="col-sm-3">
+						<div class="address">
+							<img src="images/home/map.png" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>DỊCH VỤ</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Hỗ trợ trực tuyến</a></li>
+								<li><a href="#">Liên hệ chúng tôi</a></li>
+								<li><a href="#">Tình trạng đặt hàng</a></li>
+								<li><a href="#">Thay đổi địa điểm</a></li>
+								<li><a href="#">Câu hỏi thường gặp</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>CỬA HÀNG</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Sách Thiếu Nhi</a></li>
+								<li><a href="#">Sách Giáo Dục</a></li>
+								<li><a href="#">Tiểu Thuyết</a></li>
+								<li><a href="#">Truyện Tranh</a></li>
+								<li><a href="#">Trinh Thám</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>CHÍNH SÁCH</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Điều khoản sử dụng</a></li>
+								<li><a href="#">Chính sách quyền riêng tư</a></li>
+								<li><a href="#">Chính sách hoàn tiền</a></li>
+								<li><a href="#">Hệ thống thanh toán</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>VỀ CHÚNG TÔI</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Thông tin công ty</a></li>
+								<li><a href="#">Vị trí cửa hàng</a></li>
+								<li><a href="#">Bản quyền</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>Cập Nhật Tin Tức Mới Nhất</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Địa chỉ email của bạn" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Nhận thông tin mới nhất từ <br />trang web của chúng tôi...</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2023 Sachhayhuce Inc. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
